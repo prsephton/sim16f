a38191850cfd7877f864a5b38f52cdec7a169da2
paul <prsephton@gmail.com>
2022-07-28 08:47:22 +0200

    Added Port B base definition

---
+++ src/ui/port_b0.h
@@ -0,0 +1,236 @@
+#pragma once
+
+#include <gtkmm.h>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include "paint/cairo_drawing.h"
+#include "../utils/smart_ptr.h"
+#include "paint/common.h"
+#include "paint/diagrams.h"
+
+namespace app {
+
+	class PortB0: public CairoDrawing {
+		CPU_DATA &m_cpu;
+		Glib::RefPtr<Gtk::Builder> m_refGlade;
+	  protected:
+		std::map<std::string, SmartPtr<Component> > m_components;
+	  public:
+
+		virtual bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) {
+			cr->save();
+			white(cr);
+			cr->paint();
+			black(cr);
+			show_coords(cr);
+			cr->move_to(400, 20);
+			cr->scale(2.0, 2.0);
+			cr->set_line_width(0.1);
+			cr->text_path("Device RB0/INT");
+			cr->fill_preserve(); cr->stroke();
+			cr->restore();
+			return false;
+		}
+
+		void draw_data_bus() {
+			WireDiagram &wire = dynamic_cast<WireDiagram &>(*m_components["Data Bus"]);
+			wire.add(WireDiagram::pt(0,  53, true));
+			wire.add(WireDiagram::pt(100, 53));
+			wire.add(WireDiagram::pt(70,  53, true,true));
+			wire.add(WireDiagram::pt(70, 285));
+			wire.add(WireDiagram::pt(210,285));
+			wire.add(WireDiagram::pt(70, 144, true,true));
+			wire.add(WireDiagram::pt(100,144));
+			wire.add(WireDiagram::pt(70, 250, true,true));
+			wire.add(WireDiagram::pt(120,250));
+			wire.add(WireDiagram::text(0, 51, "Data bus"));
+		}
+
+		void draw_tristate1_input() {
+			ConnectionDiagram &conn = dynamic_cast<ConnectionDiagram &>(*m_components["Tristate1 input"]);
+			conn.add(ConnectionDiagram::pt(70,53,true));
+			conn.add(ConnectionDiagram::pt(170,53));
+
+		}
+
+		void draw_tristate1_gate() {
+			ConnectionDiagram &conn = dynamic_cast<ConnectionDiagram &>(*m_components["Tristate1 gate"]);
+			conn.add(ConnectionDiagram::pt(70,145, true));
+			conn.add(ConnectionDiagram::pt(100,145));
+			conn.add(ConnectionDiagram::pt(185,145));
+			conn.add(ConnectionDiagram::pt(185, 70));
+
+			// connection to RBPU_AND
+			conn.add(ConnectionDiagram::pt(90,145,true,true));
+			conn.add(ConnectionDiagram::pt(90, -20));
+			conn.add(ConnectionDiagram::pt(170,-20));
+
+			// connection to Tristate3 input
+			conn.add(ConnectionDiagram::pt(90, 145,true));
+			conn.add(ConnectionDiagram::pt(90, 250));
+			conn.add(ConnectionDiagram::pt(50, 250));
+
+		}
+
+		void draw_pin_wire() {
+			WireDiagram &wire = dynamic_cast<WireDiagram &>(*m_components["Pin Wire"]);
+			wire.add(WireDiagram::pt(400, 145, true));
+			wire.add(WireDiagram::pt(530, 145));
+			wire.add(WireDiagram::pt(500, 145, true, true));
+			wire.add(WireDiagram::pt(500, 375));
+
+			//  TTL Input buffer
+			wire.add(WireDiagram::pt(480, 375));
+			wire.add(new BufferSymbol(480, 375, M_PI));
+
+			// Wire between input buffer and input latch
+			wire.add(WireDiagram::pt(450, 375, true));
+			wire.add(WireDiagram::pt(430, 375));
+
+			// Wire from PBPU MOS to pin horizontal
+			wire.add(WireDiagram::pt(480,  80, true));
+			wire.add(WireDiagram::pt(480, 145, false, true));
+
+		}
+
+
+		void draw_wr_portb() {
+			ConnectionDiagram &conn = dynamic_cast<ConnectionDiagram &>(*m_components["WR_PORTB"]);
+			conn.add(ConnectionDiagram::pt(0,   96, true));
+			conn.add(ConnectionDiagram::pt(100, 96));
+			conn.add(ConnectionDiagram::text(0,  94, "WR PortB"));
+		}
+
+
+		void draw_wr_trisb() {
+			ConnectionDiagram &conn = dynamic_cast<ConnectionDiagram &>(*m_components["WR_TRISB"]);
+			conn.add(ConnectionDiagram::pt(0,   66, true));
+			conn.add(ConnectionDiagram::pt(100, 66));
+			conn.add(ConnectionDiagram::text(0,  64, "WR TrisB"));
+		}
+
+
+		void draw_schmitt() {
+			ConnectionDiagram &conn = dynamic_cast<ConnectionDiagram &>(*m_components["SchmittOut"]);
+			conn.add(ConnectionDiagram::pt(  0, 45, true));
+			conn.add(ConnectionDiagram::pt(  0, 74));
+			conn.add(ConnectionDiagram::pt(-60, 74));
+		}
+
+		void draw_rd_trisb() {
+			ConnectionDiagram &conn = dynamic_cast<ConnectionDiagram &>(*m_components["RD_TRISB"]);
+			conn.add(ConnectionDiagram::pt(0,   40, true));
+			conn.add(ConnectionDiagram::pt(140, 40));
+			conn.add(ConnectionDiagram::pt(140, 30));
+			conn.add(ConnectionDiagram::text(0, 38, "RD TrisB"));
+		}
+
+		void draw_rd_portb() {
+			ConnectionDiagram &conn = dynamic_cast<ConnectionDiagram &>(*m_components["RD_PORTB"]);
+			conn.add(ConnectionDiagram::pt(0,   55, true));
+			conn.add(ConnectionDiagram::pt(225, 55));
+			conn.add(ConnectionDiagram::pt(225,-15));
+			conn.add(ConnectionDiagram::pt(225, 55, true, true));
+			conn.add(ConnectionDiagram::pt(240, 55));
+			conn.add(ConnectionDiagram::text(0, 53, "RD PortB"));
+		}
+
+		void draw_inverter1_out() {
+			ConnectionDiagram &conn = dynamic_cast<ConnectionDiagram &>(*m_components["Inverter1 out"]);
+			conn.add(ConnectionDiagram::pt(  0,  0, true));
+			conn.add(ConnectionDiagram::pt( 80,  0));
+			conn.add(ConnectionDiagram::pt( 80,-40));
+			conn.add(ConnectionDiagram::pt( 60,-40));
+		}
+
+		void draw_output_q() {
+			ConnectionDiagram &conn = dynamic_cast<ConnectionDiagram &>(*m_components["Output.Q"]);
+			conn.add(ConnectionDiagram::pt(  0,  54, true));
+			conn.add(ConnectionDiagram::pt( -20, 54));
+		}
+
+		void draw_rbpu() {
+			ConnectionDiagram &conn = dynamic_cast<ConnectionDiagram &>(*m_components["RBPU"]);
+			conn.add(ConnectionDiagram::pt(   0,  0, true));
+			conn.add(ConnectionDiagram::pt( 268,  0, false, false, true));
+			conn.add(new AndSymbol(270, 10, 0, true));
+			conn.add(ConnectionDiagram::text(0, -2, "RBPU").overscore());
+		}
+
+		void draw_rbpu_and() {
+			ConnectionDiagram &conn = dynamic_cast<ConnectionDiagram &>(*m_components["RBPU_AND"]);
+			conn.add(ConnectionDiagram::pt( 320,  10, true));
+			conn.add(ConnectionDiagram::pt( 360,  10, false, false, true));
+			conn.add(new FETSymbol(360, 10, 0, false, false, true));
+		}
+
+		void on_wire_change(Wire *wire, const std::string &name, const std::vector<BYTE> &data) {
+			m_area->queue_draw();
+		}
+
+		void on_connection_change(Connection *conn, const std::string &name, const std::vector<BYTE> &data) {
+			m_area->queue_draw();
+		}
+
+		PortB0(CPU_DATA &a_cpu, const Glib::RefPtr<Gtk::Builder>& a_refGlade):
+			CairoDrawing(Glib::RefPtr<Gtk::DrawingArea>::cast_dynamic(a_refGlade->get_object("dwg_RB0"))),
+			m_cpu(a_cpu), m_refGlade(a_refGlade)
+		{
+			auto &p0 = dynamic_cast<BasicPortB &>(*(m_cpu.portb.RB[0]));
+			auto &c = p0.components();
+			Latch &DataLatch = dynamic_cast<Latch &>(*(c["Data Latch"]));
+			Latch &TrisLatch = dynamic_cast<Latch &>(*(c["Tris Latch"]));
+			Wire &DataBus = dynamic_cast<Wire &> (*(c["Data Bus"]));
+			Wire &PinWire = dynamic_cast<Wire &> (*(c["Pin Wire"]));
+			Tristate &Tristate1 = dynamic_cast<Tristate &> (*(c["Tristate1"]));
+			Tristate &Tristate2 = dynamic_cast<Tristate &> (*(c["Tristate2"]));
+			Tristate &Tristate3 = dynamic_cast<Tristate &> (*(c["Tristate3"]));
+			Latch &OutputLatch = dynamic_cast<Latch &>(*(c["SR1"]));
+			Inverter &Inverter1 = dynamic_cast<Inverter &> (*(c["Inverter1"]));
+			Clamp &Clamp1 = dynamic_cast<Clamp &> (*(c["PinClamp"]));
+			AndGate &RBPU = dynamic_cast<AndGate &> (*(c["RBPU_NAND"]));
+
+			DeviceEvent<Wire>::subscribe<PortB0>(this, &PortB0::on_wire_change, &DataBus);
+			DeviceEvent<Connection>::subscribe<PortB0>(this, &PortB0::on_connection_change, &DataLatch.Q());
+			DeviceEvent<Connection>::subscribe<PortB0>(this, &PortB0::on_connection_change, &TrisLatch.Q());
+			DeviceEvent<Connection>::subscribe<PortB0>(this, &PortB0::on_connection_change, &Tristate1.rd());
+
+			m_components["Data Latch"] = new LatchDiagram(DataLatch, true, 200.0, 130.0, m_area);
+			m_components["Tris Latch"] = new LatchDiagram(TrisLatch, true, 200.0, 220.0, m_area);
+			m_components["Data Bus"]   = new WireDiagram( DataBus,   100.0,  90.0, m_area);
+			m_components["Pin Wire"]   = new WireDiagram( PinWire,   0.0,  0.0, m_area);
+			m_components["Tristate1"]  = new TristateDiagram( Tristate1, true, 370.0, 145.0, m_area);
+			m_components["Tristate1 input"] = new ConnectionDiagram(DataLatch.Q(), 200, 90, m_area);
+			m_components["Tristate1 gate"]  = new ConnectionDiagram(TrisLatch.Q(), 200, 90, m_area);
+			m_components["Pin"]  = new PinDiagram(p0.pin(), 530, 145, 0, 1, m_area);
+			m_components["WR_PORTB"]  = new ConnectionDiagram(DataLatch.Ck(), 100, 90, m_area);
+			m_components["WR_TRISB"]  = new ConnectionDiagram(TrisLatch.Ck(), 100, 210, m_area);
+			m_components["Tristate2"]  = new TristateDiagram(Tristate2, false, 340, 375, m_area);
+			m_components["Tristate3"]  = new TristateDiagram(Tristate3, false, 250, 340, m_area);
+			m_components["Inverter1"]  = new InverterDiagram(Inverter1, 340, 455, CairoDrawing::DIRECTION::RIGHT, m_area);
+			m_components["Output Latch"]= new LatchDiagram(OutputLatch, false, 360.0, 360.0, m_area);
+			m_components["RD_TRISB"]  = new ConnectionDiagram(Tristate3.gate(), 100, 320, m_area);
+			m_components["RD_PORTB"]  = new ConnectionDiagram(Tristate2.gate(), 100, 400, m_area);
+			m_components["Inverter1 out"]  = new ConnectionDiagram(Inverter1.rd(), 365, 455, m_area);
+			m_components["Output.Q"]= new ConnectionDiagram(OutputLatch.Q(), 360.0, 320.0, m_area);
+			m_components["Clamp"]= new ClampDiagram(Clamp1, 515.0,145.0, m_area);
+			m_components["RBPU"]= new ConnectionDiagram(p0.RBPU(), 100.0, 50.0, m_area);
+			m_components["RBPU_AND"]= new ConnectionDiagram(RBPU.rd(), 100.0, 50.0, m_area);
+
+			draw_data_bus();
+			draw_tristate1_input();
+			draw_tristate1_gate();
+			draw_pin_wire();
+			draw_wr_portb();
+			draw_wr_trisb();
+			draw_rbpu();
+			draw_rbpu_and();
+			draw_rd_trisb();
+			draw_rd_portb();
+			draw_inverter1_out();
+			draw_output_q();
+		}
+
+	};
+}
---
+++ src/ui/portb.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <gtkmm.h>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include "port_b0.h"
+//#include "port_b1.h"
+//#include "port_b2.h"
+//#include "port_b3.h"
+//#include "port_b4.h"
+//#include "port_b5.h"
+//#include "port_b6.h"
+//#include "port_b7.h"
+#include "../utils/smart_ptr.h"
+
+namespace app {
+
+	class PortB: public Component {
+		CPU_DATA &m_cpu;
+		Glib::RefPtr<Gtk::Builder> m_refGlade;
+		SmartPtr<Component> b0;
+		SmartPtr<Component> b1;
+		SmartPtr<Component> b2;
+		SmartPtr<Component> b3;
+		SmartPtr<Component> b4;
+		SmartPtr<Component> b5;
+		SmartPtr<Component> b6;
+		SmartPtr<Component> b7;
+
+	  public:
+
+		PortB(CPU_DATA &a_cpu, const Glib::RefPtr<Gtk::Builder>& a_refGlade):
+				m_cpu(a_cpu), m_refGlade(a_refGlade)
+		{
+			b0 = new PortB0(a_cpu, a_refGlade);
+//			b1 = new PortB1(a_cpu, a_refGlade);
+//			b2 = new PortB2(a_cpu, a_refGlade);
+//			b3 = new PortB3(a_cpu, a_refGlade);
+//			b4 = new PortB4(a_cpu, a_refGlade);
+//			b5 = new PortB5(a_cpu, a_refGlade);
+//			b6 = new PortB6(a_cpu, a_refGlade);
+//			b7 = new PortB7(a_cpu, a_refGlade);
+		}
+	};
+
+}
