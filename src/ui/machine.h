ce18cda5ced557ce8d5b8a13bc28ef871252b0aa
paul <prsephton@gmail.com>
2022-07-28 08:54:38 +0200

    Added PortB

--- src/ui/machine.h
+++ src/ui/machine.h
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <iomanip>
 #include "porta.h"
+#include "portb.h"
 
 namespace app {
 
@@ -15,11 +16,14 @@ namespace app {
 
 	  protected:
 		PortA port_a;
+		PortB port_b;
 	  public:
 		Machine(CPU_DATA &a_cpu, const Glib::RefPtr<Gtk::Builder>& a_refGlade):
 			m_cpu(a_cpu), m_refGlade(a_refGlade),
-			port_a(a_cpu, a_refGlade)
-		{}
+			port_a(a_cpu, a_refGlade), port_b(a_cpu, a_refGlade)
+		{
+
+		}
 	};
 
 }
