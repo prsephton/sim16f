7388f3e5310e072681223ac13d5d54121203492d
paul <prsephton@gmail.com>
2022-08-12 22:21:15 +0200

    Changed implementations for is_numeric and is_hex

--- src/utils/utility.cc
+++ src/utils/utility.cc
@@ -1,6 +1,7 @@
 #include "utility.h"
 
 #include <cstdio>
+#include <cstring>
 #include <sys/stat.h>
 
 
@@ -22,15 +23,17 @@ const std::string to_upper(std::string in) {
 }
 
 bool is_decimal(const std::string &in) {
-	char *p;
-	strtoul(in.c_str(), &p, 10);
-	return (p==NULL);
+	for (auto c: in) {
+		if (!std::isdigit(c)) return false;
+	}
+	return true;
 }
 
 bool is_hex(const std::string &in) {
-	char *p;
-	strtoul(in.c_str(), &p, 16);
-	return (p==NULL);
+	for (auto c: in) {
+		if (!std::isdigit(c) && strchr("ABCDEF", std::toupper(c))==NULL) return false;
+	}
+	return true;
 }
 
 bool FileExists(const std::string &s)
